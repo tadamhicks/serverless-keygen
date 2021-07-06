36b2ae6b6cd862d62cd2f18ddf08b0555f8e813e
Ubuntu <ubuntu@ip-172-31-1-58.ec2.internal>
2021-07-06 13:47:42 +0000

    beeline instrumentation the way I like it

--- app.py
+++ app.py
@@ -8,7 +8,13 @@ from cryptography.hazmat.primitives.asymmetric import rsa
 from cryptography.hazmat.primitives import serialization
 
 from flask import Flask, jsonify, request
+import beeline
+from beeline.middleware.flask import HoneyMiddleware
+
+beeline.init(writekey=os.environ['HONEYCOMB_API_KEY'], dataset=os.environ['HONEYCOMB_DATASET'], service_name=os.environ['SERVICE_NAME'])
+
 app = Flask(__name__)
+HoneyMiddleware(app, db_events=False)
 
 CLIENT_KEYS_TABLE = os.environ['CLIENT_KEYS_TABLE']
 client = boto3.client('dynamodb')
@@ -21,6 +27,12 @@ def hello():
 
 @app.route("/client_keys/<string:client_id>")
 def get_user(client_id):
+    beeline.add_context_field("client_id", client_id)
+    span = beeline.start_span(context={
+    "name": "db query",
+    "db_host": CLIENT_KEYS_TABLE,
+    "query": "get_item",
+    })
     resp = client.get_item(
         TableName=CLIENT_KEYS_TABLE,
         Key={
@@ -28,6 +40,8 @@ def get_user(client_id):
         }
     )
     item = resp.get('Item')
+    beeline.add_context_field("item", item)
+    beeline.finish_span(span)
     if not item:
         return jsonify({'error': 'User does not exist'}), 404
 
@@ -43,9 +57,14 @@ def get_user(client_id):
 @app.route("/client_keys", methods=["POST"])
 def create_client_keys():
     client_id = request.json.get('client_id')
+    beeline.add_context_field("client_id", client_id)
     #name = request.json.get('name')
     if not client_id:
         return jsonify({'error': 'Please provide client_id'}), 400
+
+    span = beeline.start_span(context={
+    "name": "key gen"
+    })
 
     private_key = rsa.generate_private_key(
         public_exponent=65537,
@@ -65,6 +84,13 @@ def create_client_keys():
         encoding=serialization.Encoding.PEM,
         format=serialization.PublicFormat.SubjectPublicKeyInfo
         )
+    
+    beeline.finish_span(span)
+    second_span = beeline.start_span(context={
+    "name": "db query",
+    "db_host": CLIENT_KEYS_TABLE,
+    "query": "put_item",
+    })
 
     resp = client.put_item(
         TableName=CLIENT_KEYS_TABLE,
@@ -73,6 +99,8 @@ def create_client_keys():
             'client_priv_key': {'B': priv_pem }
         }
     )
+    beeline.add_context_field("client_id", client_id)
+    beeline.finish_span(second_span)
 
     return jsonify({
         'client_id': client_id,
